0d4e9f9ae9500b7e5b976d26f5e973b4091a0de0
Prashant Suryawanshi <122711681+PrashantVS18@users.noreply.github.com>
2025-09-28 17:32:14 +0530

    Time: 95 ms (44.24%), Space: 46 MB (22.22%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        bool isPresent = false;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                isPresent = true;
+                slow = head;
+                Console.WriteLine("exist");
+                break;
+            }
+        }
+        if(isPresent)
+        {
+            while(slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+        Console.WriteLine("not exist");
+        return null;
+    }
+}
