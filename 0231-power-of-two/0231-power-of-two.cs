c224d65572f3c32b908fa6638c9990ed45215f0f
Prashant Suryawanshi <122711681+PrashantVS18@users.noreply.github.com>
2025-10-15 10:14:54 +0530

    Time: 0 ms (100%), Space: 31.4 MB (8.84%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -4,7 +4,7 @@ public class Solution {
         {
             return true;
         }
-        if((n != 1 && n%2 == 1) || n == 0)
+        if(n == 0 || n%2 != 0)
         {
             return false;
         }
