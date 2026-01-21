e527f8f097d256740c259b49457d49fc023bb551
Prashant Suryawanshi <122711681+PrashantVS18@users.noreply.github.com>
2026-01-21 23:00:11 +0530

    Time: 0 ms (100%), Space: 40 MB (32.1%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int i = s.Length - 1;
+        while(s[i] == ' ')
+        {
+            i--;
+        }
+        int length = 0;
+        while( i >= 0&&s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+
+        
+        return length;
+    }
+}
